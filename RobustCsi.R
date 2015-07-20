c22b1b8b91e12b3362dcb157abed1e5d3562d3c8
Leonard Vanbrabant <lgf.vanbrabant@gmail.com>
2015-07-20 13:28:20 +0200

    Update RobustCsi.R

--- RobustCsi.R
+++ RobustCsi.R
@@ -1,108 +1,113 @@
-#Parts of this R code are taken from the Robustbase package.
-testWaldScoresLRT <- function(y, x, beta0, beta2, sigma, Amat, meq) {
+robustCsi <- function(X, y, Amat, bvec, meq) { 
   
-  x <- as.matrix(x)
-  n <- nrow(x)
-  p <- ncol(x)
-  idx0 <- which(colSums(Amat) == 0)
-  idx1 <- c(which(colSums(Amat) == 1) , which(colSums(Amat) == -1))
+  pvalues_chi <- pvalues_f <- statistics <- rep(as.numeric(NA), 6)
+  convergence <- vector("numeric", 3)
+  names(pvalues_chi) <- names(pvalues_f) <- names(statistics) <- c("W1", "W2", "S", "Fm", "MH", "Fbar")
+  df_error <- nrow(X) - ncol(X)
+  df1a <- 0:(nrow(Amat) - meq) 
+  df1b <- meq:nrow(Amat)
   
-  #tukey bisquare tuning constant
-  c2 = 4.685061
+  n <- dim(X)[1]
+  p <- dim(X)[2]
+  #fit linear model baed on OLS estimates
+  fit.lm <- lm(y~X[,-1])
   
-  #Calculate M, Q, V 
-  res0 <- y - x%*%beta0
-  res2 <- y - x%*%beta2
-    
-  rstar0 <- res0/sigma                                                         
-  rstar2 <- res2/sigma
+  #Fbar test-statistic
+  fit.clm <- csi.lm(fit.lm, Amat=Amat, bvec=bvec, meq=meq, overall=FALSE,
+                    pvalue=TRUE, mix.weights="mvtnorm") 
   
-  psi0.c2   <- tukeyChi(rstar0, cc=c2, deriv=1)  
-  psi1.c2   <- tukeyChi(rstar2, cc=c2, deriv=1) 
-  psideriv0.c2 <- tukeyChi(rstar0, cc=c2, deriv=2) 
-  psideriv1.c2 <- tukeyChi(rstar2, cc=c2, deriv=2) 
+  pvalues_f[6] <- pvalues_chi[6] <- fit.clm$p.value[1]
+  statistics[6] <- fit.clm$T.obs[1]
   
-  #compute M 
-  weightsM <- psideriv1.c2 / sigma             
-  WM <- weightsM %*% rep(1, p)
-  xwM <- x * WM
-  M <- t(x) %*% xwM / n
+  #MM-estimator
+  rfit0 <- crlm(x=X, y=y, Amat=Amat, meq=nrow(Amat), bvec=bvec, method="MM", 
+                maxit=5000, acc = 1e-9) 
+  rfit1 <- crlm(x=X, y=y, Amat=Amat, meq=meq, bvec=bvec, method="MM", 
+                maxit=5000, acc = 1e-9)
+  rfit2 <- crlm(x=X, y=y, method="MM", maxit=5000, acc = 1e-9)
   
-  #compute Q 
-  weightsQ <- psi1.c2^2
-  WQ <- weightsQ %*% rep(1, p)
-  xwQ <- x * WQ
-  Q <- t(x) %*% xwQ / n
+  #check for convergence
+  convergence[1] <- rfit0$converged
+  convergence[2] <- rfit1$converged 
+  convergence[3] <- rfit2$converged
   
-  #Calculate V 
-  Minv <- solve(M)
-  #information matrix 
-  V <- Minv %*% Q %*% t(Minv)
-  V22 <- V[idx1,idx1]
-  #this produces problems when the constraints are not ordered!
-  #V22 <- Amat %*% V %*% t(Amat)
-  #inverse information matrix
-  #V.inv <- solve(V)
-  #submatrix of V
-  V22.inv <- solve(V22)
-#  V22.inv <- Amat %*% V.inv %*% t(Amat)
+  #regression coefficients
+  beta0 <- as.vector(round(rfit0$coefficients, 4))
+  beta1 <- as.vector(round(rfit1$coefficients, 4))
+  beta2 <- as.vector(round(rfit2$coefficients, 4))
   
-  M221 <- M[idx1,idx1] - M[idx1,idx0] %*% solve(M[idx0,idx0,drop=FALSE], 
-                                                M[idx0,idx1,drop=FALSE])
-
-#  p0 <- length(idx0)
-#  M[(p0+1):p,(p0+1):p] - M[(p0+1):p,1:p0] %*% solve(M[1:p0,1:p0,drop=FALSE], 
-#                                                    M[1:p0,(p0+1):p,drop=FALSE])
-
-
-  weightsZ <- psi0.c2 
-  Z <- t(x) %*% weightsZ / n  
-
-  #Wald-type test statistic, Silvapulle (1996, eq. 2.6)
-  Tn <- sqrt(n)*beta2[idx1]
-  Dn <- Tn
-  Dmat <- V22.inv
-  dvec <- t(Dn)%*%V22.inv
-  #constrained optimization
-  out <- quadprog:::solve.QP(Dmat=Dmat, dvec=dvec, Amat=t(Amat[,idx1]), bvec=bvec, meq=meq) 
-  b <- out$solution
-  TS_W2 <- (t(Dn)%*%Dmat%*%Dn)   
-  TS_W  <- (t(Dn)%*%Dmat%*%Dn) - (t(Dn-b)%*%Dmat%*%(Dn-b)) 
-  result_W <- n * beta2[idx1] %*% solve(V22, beta2[idx1])
+  #scale parameter
+  sigma <- rfit2$s
+  #standard deviation
+  tau.hat <- summary(rfit2)$stddev  
   
-  #Score-type test (Silvapulle, 1996, eq. 2.6)
-  An <- sqrt(n) * solve(M221) %*% Z[idx1]     
-  Dn <- An
-  dvec <- t(Dn)%*%V22.inv
-  out <- quadprog:::solve.QP(Dmat=Dmat, dvec=dvec, Amat=t(Amat[,idx1]), bvec=bvec, meq=meq)   
-  b <- out$solution
-  TS_S2 <- (t(Dn)%*%Dmat%*%Dn)
-  TS_S <- (t(Dn)%*%Dmat%*%Dn) - (t(Dn-b)%*%Dmat%*%(Dn-b))
-
-  #Score-type test, extention Markautou and Hettmansperger (1990) (see Silvapulle, 1996)
-  #result.u <- M221 %*% V[idx1,idx1] %*% t(M221)
-  result.u <- Q[idx1,idx1]-M[idx1,idx0]%*% solve(M[idx0,idx0,drop=F], Q[idx0,idx1,drop=F]) - 
-              Q[idx1,idx0]%*% solve(M[idx0,idx0,drop=F], M[idx0,idx1,drop=F])+
-              M[idx1,idx0]%*% solve(M[idx0,idx0,drop=F], Q[idx0,idx0,drop=F])%*%
-               solve(M[idx0,idx0,drop=F], M[idx0,idx1,drop=F])
+  #R-square and R-square adjusted (see lmrob.R)
+  #    fit.rob <- lmrob(y~X[,-1])
+  #    summary(fit.rob)
   
-  U <- 1/sqrt(n) * solve(M221) %*% Z[idx1]     
-  A.tilde <- solve(M221) %*% result.u %*% t(solve(M221))
-  Dmat <- A.tilde
-  dvec <- t(U)%*%A.tilde
-  out <- quadprog:::solve.QP(Dmat=Dmat, dvec=dvec, Amat=t(Amat[,idx1]), bvec=bvec, meq=meq)   
-  b <- out$solution
-  TS_MH <- (t(U)%*%Dmat%*%U) - (t(U-b)%*%Dmat%*%(U-b)) 
+  #compute std.error
+  #    se <- sqrt(diag(vcov.MM(rfit.h1)))
   
-  return(list(TS_W = TS_W,          
-              TS_W2 = TS_W2,
-              result_W = result_W,
-              TS_S = TS_S,
-              TS_MH = TS_MH,
-              TS_S2 = TS_S2,
-              V22 = V22
-             )
-        )
   
+  ## robust Wald (W1, W2) and score test-statistics ##
+  TS <- testWaldScoresLRT(y=y, x=X, beta0, beta2, sigma, Amat, meq=meq)
+  statistics[c(2,3,5)] <- unlist(TS)[c("TS_W", "TS_S", "TS_MH")]
+  
+  cov <- TS$V22
+  
+  wt <- function(cov, meq) {
+    if (meq == 0L) {
+      wt.bar <- ic.infer:::ic.weights(cov)
+    }
+    else if (meq > 0) {
+      wt.bar <- ic.infer:::ic.weights(solve(solve(cov)[-(1:meq), -(1:meq)]))
+    }
+    wt.bar
+  }
+  
+  wt_bar <- wt(cov, meq=meq)
+  #null distribution mixture of chi^2 distributions
+  pvalues_chi[c(2,3,5)] <- sapply(statistics[c(2,3,5)], function(x) 1-pchibarA(x, df1a=df1a, wt=rev(wt_bar))) 
+  #null distribution mixture of F distributions
+  pvalues_f[c(2,3,5)] <- sapply(statistics[c(2,3,5)], function(x) 1-pfbarA(x, df1a=df1a, df2a=df_error, wt=rev(wt_bar)))  
+  
+  
+  ## robust Fm test statistic ##
+  #tuning constant bisquare weight
+  cc <- 4.685061
+  #compute residuals under h0 and h1
+  resid0 <- y - X%*%beta0
+  resid1 <- y - X%*%beta1
+  
+  #residuals / scale
+  rstar0 <- c(resid0/sigma)                                               
+  rstar1 <- c(resid1/sigma)
+  
+  L0 <- sum(tukeyChi(rstar0, cc, deriv = 0))
+  L1 <- sum(tukeyChi(rstar1, cc, deriv = 0))
+  
+  #first derivative psi function
+  psi.prime.h1 <- tukeyChi(rstar1, cc, deriv=1)                     
+  #second derivative psi function
+  psi.prime2.h1 <- tukeyChi(rstar1, cc, deriv=2)                    
+  
+  #asymptotic covariance matrix standardizing constant
+  l.h1 <- ( 0.5 * (1/(n - p)) * sum(psi.prime.h1^2) ) / ( (1/n)*sum(psi.prime2.h1) )
+  statistics[4] <- 1/l.h1 * (L0 - L1) 
+  
+  wt_bar <- wt(cov=Amat%*%(solve(t(X)%*%X))%*%t(Amat), meq=meq)
+  pvalues_chi[4] <- 1-pchibarA(x=statistics[4], df1a=df1a, wt=rev(wt_bar))
+  pvalues_f[4] <- 1-pfbarA(x=statistics[4], df1a=df1a, df2a=df_error, wt=rev(wt_bar))  
+  
+  ## robust Wald statistic, Silvapulle (1992) ##
+  niX <- X
+  statistics[1] <- ( (t(beta2-beta0)%*%(t(niX)%*%niX)%*%(beta2-beta0)) - 
+                       (t(beta2-beta1)%*%(t(niX)%*%niX)%*%(beta2-beta1)) ) / tau.hat^2 
+  
+  pvalues_chi[1] <- 1-pchibarA(x=statistics[1], df1a=df1a, wt=rev(wt_bar))
+  pvalues_f[1]   <- 1-pfbarA(x=statistics[1], df1a=df1a, df2a=df_error, wt=rev(wt_bar))
+  
+  OUT <- data.frame(statistics=statistics, pvalues_chi=pvalues_chi, pvalues_f=pvalues_f)
+  
+  OUT
 }
-
