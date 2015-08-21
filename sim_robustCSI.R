af936e484e25868692d67856357ad2aa1d75d26d
Leonard Vanbrabant <lgf.vanbrabant@gmail.com>
2015-08-21 13:54:33 +0200

    Update sim_robustCSI.R

--- sim_robustCSI.R
+++ sim_robustCSI.R
@@ -1,5 +1,5 @@
 sim_robustCsi <- function(constraints=NULL, 
-                          N=c(15,25,50), d=0, eta.vec=12, cont=0.10, cont.x=2, 
+                          N=c(15,25,50), d=0, eta.vec=12, cont=0.10, cont.x=1, 
                           R=999, ncpus=1, seed=123, verbose=TRUE) {
   
   #checks
@@ -20,7 +20,7 @@ sim_robustCsi <- function(constraints=NULL,
   
   for(j in 1:S) {
     if(length(N) > 1) { n <- N[j] } else { n <- N }
-    if(length(d) > 1) { betas <- c(1,1,d[j],d[j],d[j]) } else { betas <- c(1,1,d,d,d)}
+    if(length(d) > 1) { betas <- c(1,d[j],d[j],d[j]) } else { betas <- c(1,d,d,d)}
     if(length(eta.vec) > 1) { eta <- eta.vec[j] } else { eta <- eta.vec }
     
     statistics <- pvalues_chi <- pvalues_f <- matrix(NA, R, 4)
@@ -29,14 +29,14 @@ sim_robustCsi <- function(constraints=NULL,
     for(i in 1:R) {
       set.seed(seed+i)
       if(verbose) { cat("iteration:", j, "...", i, "\n") }
-      X <- cbind(1, mvtnorm:::rmvnorm(n, mean=rep(0,4), sigma=diag(4)))
+      X <- mvtnorm:::rmvnorm(n, mean=rep(0,4), sigma=diag(4))
       y <- cbind(c(betas%*%t(X))) + rnorm(n)   
       
       idx <- sample(1:nrow(y), n*cont, replace=FALSE)                             
       X[idx,cont.x] <- rnorm(length(idx), 5, 0.1)  
       y[idx] <- rnorm(length(idx), eta, 0.1)  
 
-      Data <- data.frame(y=y, X=X[,-1])
+      Data <- data.frame(y=y, X=X)
         colnames(Data) <- c("y","x1","x2","x3","x4")
             
       object <- MASS:::rlm(y~x1+x2+x3+x4, data=Data, method="MM", maxit=5000)
@@ -54,7 +54,7 @@ sim_robustCsi <- function(constraints=NULL,
       bvec <- res_constraints_rhs_bvec(object, constraints=constraints)
       
       
-      out <- restriktor(object, constraints=constraints, control=list(maxit=5000))
+      out <- restriktor:::restriktor(object, constraints=constraints, control=list(maxit=5000))
       statistics[i,1:4]  <- out$statistics
       pvalues_chi[i,1:4] <- out$pvalues_chi
       pvalues_f[i,1:4]   <- out$pvalues_f
